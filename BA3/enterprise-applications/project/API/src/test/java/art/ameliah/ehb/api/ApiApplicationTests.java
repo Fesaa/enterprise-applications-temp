9ad30ceb5fb169b96fe73ecb929cff29dcd4e74c
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-09-30 13:29:05 +0200

    Sping init

---
+++ BA3/enterprise-applications/project/API/src/test/java/art/ameliah/ehb/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package art.ameliah.ehb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
