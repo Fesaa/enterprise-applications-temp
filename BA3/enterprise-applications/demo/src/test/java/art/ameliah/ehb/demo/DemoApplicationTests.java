7a54dd91b99a13a0ad62d7ffc3f4703183dcfe1d
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-09-30 10:56:42 +0200

    Class demo

---
+++ BA3/enterprise-applications/demo/src/test/java/art/ameliah/ehb/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package art.ameliah.ehb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
