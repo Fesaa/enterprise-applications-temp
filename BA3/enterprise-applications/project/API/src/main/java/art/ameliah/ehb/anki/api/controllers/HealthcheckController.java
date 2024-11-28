e1996bb6b1a77e441bd8f5b207ece5f7eba1e616
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-11-28 11:54:41 +0100

    Fix health controller
    Was throwing an error because BaseController also registered something AllowAnonymous did.

--- BA3/enterprise-applications/project/API/src/main/java/art/ameliah/ehb/anki/api/controllers/HealthcheckController.java
+++ BA3/enterprise-applications/project/API/src/main/java/art/ameliah/ehb/anki/api/controllers/HealthcheckController.java
@@ -1,14 +1,14 @@
 package art.ameliah.ehb.anki.api.controllers;
 
 import art.ameliah.ehb.anki.api.annotations.AllowAnonymous;
-import art.ameliah.ehb.anki.api.annotations.BaseController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @AllowAnonymous
-@BaseController
+@RestController
 public class HealthcheckController {
 
     @GetMapping("/health")
